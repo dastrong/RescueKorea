1dbb55a0b38c546821a372d34c2a1366fe06c2e0
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-09 20:36:11 +0900

    moved logic around to create card placeholder for a better loading experience

--- client/src/components/reusable/CardComponent.js
+++ client/src/components/reusable/CardComponent.js
@@ -1,40 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Icon, Segment, Header, Button, Grid } from "semantic-ui-react";
-import ImgWithPlaceHolder from "./ImgWithPlaceholder";
+import { Card, Segment, Header, Button, Grid } from "semantic-ui-react";
 import "./CardComponent.css";
 
-const CardComponent = ({
-  image,
-  petName,
-  gender,
-  location,
-  id,
-  mobile,
-  tablet,
-  computer,
-}) => (
-  <Grid.Column key={id} mobile={mobile} tablet={tablet} computer={computer}>
+const CardComponent = ({ image, petName, gender, location, button }) => (
+  <Grid.Column mobile={16} tablet={8} computer={5}>
     <Segment inverted color="pink" className="individual-card">
       <Card fluid>
-        <ImgWithPlaceHolder src={image.url} alt="Korean pet for adoption - main image" />
+        {image}
         <Card.Content>
           <Header as="h2" color="violet" content={petName} />
-          <Card.Description>
-            <Icon name={gender.toLowerCase()} color="violet" />
-            {gender}
-          </Card.Description>
-          <Card.Description>
-            <Icon name="map marker alternate" color="violet" />
-            {location}
-          </Card.Description>
+          <Card.Description>{gender}</Card.Description>
+          <Card.Description>{location}</Card.Description>
         </Card.Content>
         <Button
           attached="bottom"
           as={Link}
-          to={`/listing/${id}`}
           color="olive"
-          content={`View ${petName}`}
+          style={{ height: "36px" }}
+          {...button}
         />
       </Card>
     </Segment>
--- client/src/components/reusable/CardList.js
+++ client/src/components/reusable/CardList.js
@@ -1,16 +1,51 @@
 import React from "react";
-import { Grid } from "semantic-ui-react";
+import { Grid, Placeholder, Icon } from "semantic-ui-react";
 import CardComponent from "./CardComponent";
-import BlurredLoader from "./BlurredLoader";
+import ImgWithPlaceHolder from "./ImgWithPlaceholder";
 
-const CardList = ({ pets, isLoading, ...sizes }) => (
-  <BlurredLoader isLoading={isLoading}>
-    <Grid style={{ justifyContent: "center" }}>
-      {pets.map(({ _id, images, ...rest }) => (
-        <CardComponent key={_id} id={_id} image={images[0]} {...rest} {...sizes} />
-      ))}
-    </Grid>
-  </BlurredLoader>
+const LoaderLine = ({ length }) => (
+  <Placeholder>
+    <Placeholder.Line length={length} />
+  </Placeholder>
 );
 
-export default CardList;
+export default ({ showPlaceholders, listings }) => (
+  <Grid style={{ justifyContent: "center" }}>
+    {showPlaceholders
+      ? Array.from(Array(6)).map((q, i) => (
+          <CardComponent
+            key={`temp-${i}`}
+            image={
+              <Placeholder style={{ maxWidth: "100%" }}>
+                <Placeholder.Image rectangular />
+              </Placeholder>
+            }
+            petName={<LoaderLine length="long" />}
+            gender={<LoaderLine length="short" />}
+            location={<LoaderLine length="medium" />}
+            button={{ to: "#", loading: true, content: "" }}
+          />
+        ))
+      : listings.map(({ _id, images, petName, location, gender }) => (
+          <CardComponent
+            key={_id}
+            id={_id}
+            image={<ImgWithPlaceHolder src={images[0].url} alt={petName} />}
+            petName={petName}
+            gender={
+              <>
+                <Icon name={gender.toLowerCase()} color="violet" />
+                {gender}
+              </>
+            }
+            location={
+              <>
+                <Icon name="map marker alternate" color="violet" />
+                {location}
+              </>
+            }
+            button={{ to: `/listing/${_id}`, loading: false, content: `View ${petName}` }}
+          />
+        ))}
+  </Grid>
+);
