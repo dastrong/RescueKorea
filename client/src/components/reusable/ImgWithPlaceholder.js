94fa665a40c70fd9dbe49f704d83b5033f717c7d
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-09 20:33:59 +0900

    converted to a hook

--- client/src/components/reusable/ImgWithPlaceholder.js
+++ client/src/components/reusable/ImgWithPlaceholder.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { useState } from "react";
 import { Image, Placeholder } from "semantic-ui-react";
 import "./ImgWithPlaceholder.css";
 
@@ -6,24 +6,38 @@ import "./ImgWithPlaceholder.css";
 // accepts src and alt as props
 // shows a loading placeholder until image is completely loaded
 
-class ImgWithPlaceHolder extends Component {
-  state = { isLoading: true };
+function ImgWithPlaceHolder(props) {
+  const [isLoading, toggle] = useState(true);
 
-  handleLoading = () => this.setState({ isLoading: false });
-
-  render() {
-    const { isLoading } = this.state;
-    return (
-      <Fragment>
-        <Image {...this.props} hidden={isLoading} onLoad={this.handleLoading} />
-        {isLoading && (
-          <Placeholder style={{ maxWidth: "100%" }}>
-            <Placeholder.Image rectangular />
-          </Placeholder>
-        )}
-      </Fragment>
-    );
-  }
+  return (
+    <>
+      <Image {...props} hidden={isLoading} onLoad={() => toggle(false)} />
+      {isLoading && (
+        <Placeholder style={{ maxWidth: "100%" }}>
+          <Placeholder.Image rectangular />
+        </Placeholder>
+      )}
+    </>
+  );
 }
+// class ImgWithPlaceHolder extends Component {
+//   state = { isLoading: true };
+
+//   handleLoading = () => this.setState({ isLoading: false });
+
+//   render() {
+//     const { isLoading } = this.state;
+//     return (
+//       <Fragment>
+//         <Image {...this.props} hidden={isLoading} onLoad={this.handleLoading} />
+//         {isLoading && (
+//           <Placeholder style={{ maxWidth: "100%" }}>
+//             <Placeholder.Image rectangular />
+//           </Placeholder>
+//         )}
+//       </Fragment>
+//     );
+//   }
+// }
 
 export default ImgWithPlaceHolder;
