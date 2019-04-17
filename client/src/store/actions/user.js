b25603bb9b1f45e738593549cb98c45450859117
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-17 22:36:24 +0900

    WIP - fixed pre commit issues

--- client/src/store/actions/user.js
+++ client/src/store/actions/user.js
@@ -1,1 +1,10 @@
-import {} from "../actionTypes";
+import { SET_USER, REMOVE_USER } from "../actionTypes";
+
+export const handleLogin = user => ({
+  type: SET_USER,
+  user,
+});
+
+export const handleLogout = () => ({
+  type: REMOVE_USER,
+});
--- client/src/store/reducers/user.js
+++ client/src/store/reducers/user.js
@@ -1,12 +1,24 @@
-import {} from "../actionTypes";
+import { SET_USER, REMOVE_USER } from "../actionTypes";
 
 const initialState = {
   isAuthenticated: null,
-  email: "",
+  user: {},
+  // user: {
+  //   email: "",
+  //   fullName: "",
+  //   isAdmin: false,
+  //   token: "",
+  //   userId: "",
+  // },
 };
 
 export default (state = initialState, action) => {
-  switch (action.type) {
+  const { type, user } = action;
+  switch (type) {
+    case SET_USER:
+      return { isAuthenticated: true, user };
+    case REMOVE_USER:
+      return { isAuthenticated: false, user: {} };
     default:
       return state;
   }
