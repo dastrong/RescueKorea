cc10b6c4ae577730496405fc08372acba4a0b01e
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-20 20:18:56 +0900

    testing new image

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,6 +11,10 @@ export default () => (
     <Helmet>
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Rescue Korea" />
+      <meta
+        property="og:image"
+        content="https://res.cloudinary.com/dastrong/image/upload/f_auto,h_200,q_87,w_240/v1554288174/petChingus/UX/faviconRK.png"
+      />
     </Helmet>
     <NavBar />
     <Routes />
