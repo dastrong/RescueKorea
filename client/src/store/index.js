2ab4e7277efddeac1c978c8ca6f3fc993f849807
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-20 19:43:45 +0900

    comment out redux devTools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -3,8 +3,8 @@ import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
 const middleware = compose(
-  applyMiddleware(thunk),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 export default createStore(rootReducer, middleware);
