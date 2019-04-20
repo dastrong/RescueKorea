edad140a5f2f33daed3d78a3fde0c1794c766eef
Daniel Strong <danielstrong.ds@gmail.com>
2019-04-20 20:08:29 +0900

    removed tags from index and added common tags to App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Footer from "./_static/Footer";
 import Disclaimer from "./_static/Disclaimer";
 import Routes from "./Routes";
@@ -7,6 +8,10 @@ import ScrollToTop from "./ScrollToTop";
 
 export default () => (
   <>
+    <Helmet>
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Rescue Korea" />
+    </Helmet>
     <NavBar />
     <Routes />
     <Disclaimer />
